f2cb1d523e6608eb5e18ff302b47d98298325cec
jotkate <janusz.tarasewicz@gmail.com>
2018-10-26 16:42:46 +0200

    BPN_RPZ-st10

---
+++ Stanowisko 10 - BPN/Assets/Scripts/OnMouseOverExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OnMouseOverExample : MonoBehaviour {
+
+	/* Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+*/
+	void OnMouseOver()
+	{
+		Debug.Log ("mouse over");
+	}
+
+	void OnMouseExit()
+	{
+		Debug.Log ("mouse exit");
+	}
+}
---
+++ Stanowisko 10 - BPN/Assets/Scripts/puzzle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class puzzle : MonoBehaviour {
+	public bool correctPos = false;
+	public bool mouseIsOverPuzzle = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (!correctPos && Input.GetMouseButton(0) && mouseIsOverPuzzle) {
+			transform.position = Input.mousePosition;
+		}
+
+		
+	}
+
+	void OnMouseOver()
+	{
+		mouseIsOverPuzzle = true;
+		Debug.Log ("mouse is over puzzle");
+	}
+
+	void OnMouseExit()
+	{
+		mouseIsOverPuzzle = false;
+		Debug.Log ("mouse is gone");
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		Debug.Log ("Collision detected");
+		if (gameObject.name == coll.gameObject.name) {
+			correctPos = true;
+			transform.position = coll.transform.position;
+		}
+	}
+}
